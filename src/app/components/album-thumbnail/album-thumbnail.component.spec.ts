b53830c4b9322fe401a8fbde1bb59ecf615ae8fa
Axel Leroy <axeleroy@users.noreply.github.com>
2019-10-20 22:45:23 +0200

    Rename Album and Picture to AlbumThumbnail and PictureThumbnail

--- src/app/components/album-thumbnail/album-thumbnail.component.spec.ts
+++ src/app/components/album-thumbnail/album-thumbnail.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AlbumComponent } from './album.component';
+import { AlbumThumbnailComponent } from './album-thumbnail.component';
 
 describe('AlbumComponent', () => {
-  let component: AlbumComponent;
-  let fixture: ComponentFixture<AlbumComponent>;
+  let component: AlbumThumbnailComponent;
+  let fixture: ComponentFixture<AlbumThumbnailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AlbumComponent ]
+      declarations: [ AlbumThumbnailComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AlbumComponent);
+    fixture = TestBed.createComponent(AlbumThumbnailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
