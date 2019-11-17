3bfd0c058a72fdbbd9cfeb99e0aea22899c64f1f
Axel Leroy <axeleroy@users.noreply.github.com>
2019-11-17 18:00:47 +0100

    Made caching more generic in order to cache Album list as well.

--- src/app/services/album/album.service.ts
+++ src/app/services/album/album.service.ts
@@ -1,56 +1,32 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
-import {from, Observable, of} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import AlbumContent from '../../types/AlbumContent';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError} from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
-import Album from '../../types/Album';
-import {CachedAlbum, db} from '../../AppDatabase';
+import {db} from '../../cache/app-database';
+import {CachedRequest} from '../../cache/cached-request';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AlbumService implements Resolve<Album> {
-  thirtyDaysAgo = new Date();
+export class AlbumService implements Resolve<AlbumContent> {
 
   constructor(private http: HttpClient,
               private router: Router) {
-    this.thirtyDaysAgo.setDate(this.thirtyDaysAgo.getDate() - 30);
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Album> {
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<AlbumContent> {
     const albumId = route.paramMap.get('albumId');
-    return from(db.albums.get(albumId))
-        .pipe(
-          switchMap((cachedAlbum: CachedAlbum) => {
-            // If the DB returns the album and is not too old, return it
-            if (cachedAlbum && cachedAlbum.fetchDate > this.thirtyDaysAgo) {
-              return of(cachedAlbum);
-            } else {
-              // Else, fetch the album
-              return this.http.get<AlbumContent>(environment.albumBaseUrl + albumId + environment.albumFileNameUrl);
-            }
-          }),
-          map((album: CachedAlbum | AlbumContent) => {
-            // If from db, return it, else it has just been fetched, so save it
-            if ((album as CachedAlbum).album) {
-              return (album as CachedAlbum).album;
-            } else {
-              db.albums.add({
-                id: album.id,
-                album: (album as AlbumContent),
-                fetchDate: new Date()
-              });
-
-              return album;
-            }
-          }),
-          catchError(() => {
-            this.router.navigateByUrl('/not-found');
-            return of(null);
-          })
-        );
+    return new CachedRequest<AlbumContent>(this.http)
+    .fetch(db.albums, albumId, environment.albumBaseUrl + albumId + environment.albumFileNameUrl, 30)
+    .pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/not-found');
+        return of(null);
+      })
+    );
   }
 
 }
