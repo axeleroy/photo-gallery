d0e3be1e7fe3559239138048c7a9154db71615e2
axeleroy <axeleroy@users.noreply.github.com>
2019-11-23 22:36:58 +0100

    Updated album creation script to resize pictures + upload directly the pictures without writing them to the filesystem

--- scripts/add-album.py
+++ scripts/add-album.py
@@ -1,6 +1,7 @@
 import argparse
 import boto3
 from botocore.client import ClientError
+from io import BytesIO
 import os
 import json
 import stringcase
@@ -9,12 +10,18 @@ from wand.image import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--albums-list", help="Path to the JSON file containing the albums list. If it is the first time "
-                                         "you are running this script, you set the path to a non-existant file and "
-                                         "it will create the JSON file for you", required=True)
+                                          "you are running this script, you set the path to a non-existant file and "
+                                          "it will create the JSON file for you", required=True)
 parser.add_argument("--album-folder", help="Path to the folder containing the pictures you want to create the album "
                                            "from", required=True)
 parser.add_argument("--album-name", help="Name of the new album", required=True)
 parser.add_argument("--bucket-name", help="Name of AWS S3 bucket the album will be uploaded to", required=True)
+parser.add_argument("--fullsize-ratio", type=float, help="If set, the ratio at which the fullsize pictures are "
+                                                         "reduced. For example, 0.3 will reduce a 24MP picture 30% "
+                                                         "into a ~8MP picture.")
+parser.add_argument("--thumbnail-ratio", type=float, default=0.1, help="Ratio at which the thumbnail pictures are "
+                                                                       "reduced from the original picture. Default"
+                                                                       "is 0.1 (10 percent the original size)")
 
 args = parser.parse_args()
 
@@ -27,11 +34,13 @@ if not os.path.isdir(args.album_folder):
     print(f'ERROR: {args.album_folder} is not a folder')
     exit(1)
 
+# Init s3 resource
+s3 = boto3.resource('s3')
 
 # Check that bucket exists
-s3 = boto3.client('s3')
+s3_client = boto3.client('s3')
 try:
-    s3.head_bucket(Bucket=args.bucket_name)
+    s3_client.head_bucket(Bucket=args.bucket_name)
 except ClientError:
     print(f'Bucket {args.bucket_name} does not exist or you do not have access to it')
     exit(1)
@@ -50,15 +59,6 @@ if not len(pictures):
     exit(1)
 
 
-print(f'Creating thumbnail folder at {args.album_folder}/thumbnails')
-try:
-    os.mkdir(args.album_folder + "/thumbnails")
-except FileExistsError:
-    print(f'Folder {args.album_folder}/thumbnails already exists, should we continue anyway? (y/n)')
-    continuing = input()
-    if continuing.capitalize() != "Y":
-        exit(1)
-
 album_json = {
   'id': album_id,
   'name': args.album_name,
@@ -70,58 +70,69 @@ s3Url = f'https://{args.bucket_name}.s3.amazonaws.com/{album_id}'
 
 for picture in pictures:
     picture_filename = os.path.basename(picture)
-    print(f'Uploading {picture_filename} to S3')
-    s3.upload_file(picture, args.bucket_name, f'{album_id}/{picture_filename}',
-                   ExtraArgs={'ACL': 'public-read'})
 
-    print(f'Creating thumbnail for {picture_filename}')
-
-    thumbnail_path = f'{args.album_folder}/thumbnails/{picture_filename}'
-
-    # Create the picture thumbnail at 15% size
+    # Open the picture using Wand / ImageMagick
     with Image(filename=picture) as img:
-        with img.clone() as i:
-            height = i.height
-            width = i.width
-            thumbnail_height = int(i.height * 0.15)
-            thumbnail_width = int(i.width * 0.15)
+        height = img.height
+        width = img.width
+        print(f'\nWorking on image {counter} out of {len(pictures)}')
 
-            i.resize(thumbnail_width, thumbnail_height)
-            i.save(filename=thumbnail_path)
+        if args.fullsize_ratio:
+            print(f'Reducing {picture_filename} to {int(args.fullsize_ratio * 100)} %')
+            with img.clone() as fullsize:
+                height = int(height * args.fullsize_ratio)
+                width = int(width * args.fullsize_ratio)
 
-    print(f'Uploading thumbnail to S3')
-    s3.upload_file(thumbnail_path, args.bucket_name, f'{album_id}/thumbnails/{picture_filename}',
-                   ExtraArgs={'ACL': 'public-read'})
+                fullsize.resize(width, height)
+                print(f'Uploading {picture_filename} to S3')
+                s3_client.upload_fileobj(
+                    BytesIO(fullsize.make_blob('jpeg')),
+                    args.bucket_name,
+                    f'{album_id}/{picture_filename}',
+                    ExtraArgs={'ACL': 'public-read'})
+        else:
+            print(f'Uploading {picture_filename} to S3')
+            s3_client.upload_file(
+                picture,
+                args.bucket_name,
+                f'{album_id}/{picture_filename}',
+                ExtraArgs={'ACL': 'public-read'})
 
-    print(f'Adding {picture_filename} to album\'s JSON')
-    album_json['pictures'].append({
-      'id': "%d" % counter,
-      'thumbnail': {
-          'url': f'{s3Url}/thumbnails/{picture_filename}',
-          'height': thumbnail_height,
-          'width': thumbnail_width
-      },
-      'fullsize': {
-          'url': f'{s3Url}/{picture_filename}',
-          'height': height,
-          'width': width
-      }
-    })
+        with img.clone() as thumbnail:
+            print(f'Creating a thumbnail {int(args.thumbnail_ratio * 100)} % the size of the original')
+            thumbnail_height = int(img.height * args.thumbnail_ratio)
+            thumbnail_width = int(img.width * args.thumbnail_ratio)
+
+            thumbnail.resize(thumbnail_width, thumbnail_height)
+            print(f'Uploading thumbnail to S3')
+            s3_client.upload_fileobj(
+                BytesIO(thumbnail.make_blob('jpeg')),
+                args.bucket_name,
+                f'{album_id}/thumbnails/{picture_filename}',
+                ExtraArgs={'ACL': 'public-read'})
+
+        print(f'Adding {picture_filename} to album\'s JSON')
+        album_json['pictures'].append({
+            'id': f'counter',
+            'thumbnail': {
+                'url': f'{s3Url}/thumbnails/{picture_filename}',
+                'height': thumbnail_height,
+                'width': thumbnail_width
+            },
+            'fullsize': {
+                'url': f'{s3Url}/{picture_filename}',
+                'height': height,
+                'width': width
+            }
+        })
     counter += 1
 
-print("Saving album's JSON file")
-# Save JSON containing the album's picture
-with open(f'{args.album_folder}/album.json', 'w+', encoding='utf-8') as out_file:
-    json.dump(album_json, out_file, ensure_ascii=False)
-out_file.close()
+print("\nUploading the album's JSON file to S3")
+json_obj = s3.Object(args.bucket_name, f'{album_id}/album.json')
+json_obj.put(Body=json.dumps(album_json), ACL='public-read')
 
 
-print("Uploading the album's JSON file to S3")
-s3.upload_file(f'{args.album_folder}/album.json', args.bucket_name, f'{album_id}/album.json',
-               ExtraArgs={'ACL': 'public-read'})
-
-
-print(f'Append album to {args.albums_list}')
+print(f'\nAppending album to {args.albums_list}')
 try:
     with open(args.albums_list, 'r', encoding='utf-8') as albums_list_file:
         albums_list = json.load(albums_list_file)
@@ -149,6 +160,5 @@ with open(args.albums_list, 'w+', encoding='utf-8') as albums_list_file:
     json.dump(new_album, albums_list_file, ensure_ascii=False)
 albums_list_file.close()
 
-print("Album creation done!")
+print("\nAlbum creation done!")
 print(f'Do not forget to upload {args.albums_list}')
-print(f'You might want to delete {args.album_folder}/thumbnails from your filesystem')
