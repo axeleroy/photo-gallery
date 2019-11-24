2f0c178118cd86df90df94d71f2146cf00c6cd64
axeleroy <axeleroy@users.noreply.github.com>
2019-11-24 22:36:59 +0100

    Optimized imports and removed remaining tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AlbumListPageComponent} from './pages/album-list-page/album-list-page.component';
 import {AlbumPageComponent} from './pages/album-page/album-page.component';
 import {AlbumService} from './services/album/album.service';
