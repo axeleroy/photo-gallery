184bbb37f6723a89b7b108e8b4aac5e58bafcbe6
axeleroy <axeleroy@users.noreply.github.com>
2020-01-01 22:28:25 +0100

    Added documentation to AppConfig

--- src/environments/AppConfig.ts
+++ src/environments/AppConfig.ts
@@ -1,8 +1,24 @@
 export interface AppConfig {
+  /**
+   * If set, the title of the main page.
+   */
   albumListPageTitle?: string
+  /**
+   * URL to the albums list JSON.
+   */
   albumListUrl: string;
+  /**
+   * URL to the folder containing the albums to display.
+   */
   albumBaseUrl: string;
+  /**
+   * Name of the album JSON file, with file extension.
+   * NOTE: for a given album, the app will fetch the JSON at albumBaseUrl + albumId + albumFileNameUrl
+   */
   albumFileNameUrl: string;
+  /**
+   * Whether to use Hash URL instead of HTML5 pushState
+   */
   useHash: boolean;
   isProduction: boolean;
 }
